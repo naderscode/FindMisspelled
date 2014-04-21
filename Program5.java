c6363c4ae9a92a4dee9e9940e16bf73324e2bf6b
naderscode <naderscode@gmail.com>
2014-04-21 15:35:09 -0400

    edit program5.java

--- Program5.java
+++ Program5.java
@@ -3,6 +3,7 @@ Written by Nader K
 Date 4/15/06
 FindMispelledWords
 */
+
 import java.io.*;
 import java.util.*;
 
@@ -11,59 +12,61 @@ public class Program5
 {
 	public static void main(String [] args) 
 	{
-		try
-      {
-         BufferedReader brDict = new BufferedReader(new FileReader("words.txt"));
-         BufferedReader brStory = new BufferedReader(new FileReader("story.txt"));
-      			
-         TreeSet<String> dict = new TreeSet<String>();
-        	
-      	System.out.println("Misspelled words: "); 
-			
-			String dline = brDict.readLine();
-			while(dline != null)
-			{
-					StringTokenizer sts = new StringTokenizer(dline);
-					
-					while(sts.hasMoreTokens())
-					{
-						String word = sts.nextToken().toLowerCase().trim();
-										
-						if(!dict.contains(word))
-						   dict.add(word);
-							}//inner loop
-					
-   	  				 dline = brDict.readLine();
-						 
-			}//outer loop
-			
-			/** print the TreeSet elements */
-		//	System.out.println(dict +"\n");
-	
-			String sline = brStory.readLine();
-			while(sline != null)
-			{
-					StringTokenizer s = new StringTokenizer(sline);
-					
-					while(s.hasMoreTokens())
-					{
-						String wordToCheck = s.nextToken().toLowerCase().trim();
-										
-						if(!dict.contains(wordToCheck))
-						  	System.out.println(wordToCheck);
-							}//inner loop
-					
-   	  				 sline = brStory.readLine();
-						 
-			}//outer loop
-
-      
-      }//end try
-      catch (IOException e)
-      {
-         System.out.println("Error: " + e);
-      }
-		
+      try
+          {
+             BufferedReader brDict = new BufferedReader(new FileReader("words.txt"));
+             BufferedReader brStory = new BufferedReader(new FileReader("story.txt"));
+                    
+             TreeSet<String> dict = new TreeSet<String>();
+                
+            System.out.println("Misspelled words: "); 
+                
+                String dline = brDict.readLine();
+                while(dline != null)
+                {
+                        StringTokenizer sts = new StringTokenizer(dline);
+                        
+                        while(sts.hasMoreTokens())
+                        {
+                            String word = sts.nextToken().toLowerCase().trim();
+                                            
+                            if(!dict.contains(word))
+                               dict.add(word);
+                                }//inner loop
+                        
+                         dline = brDict.readLine();
+                             
+                }//outer loop
+                
+                /** print the TreeSet elements */
+            //	System.out.println(dict +"\n");
+        
+                String sline = brStory.readLine();
+                while(sline != null)
+                {
+                        StringTokenizer s = new StringTokenizer(sline);
+                        
+                        while(s.hasMoreTokens())
+                        {
+                            String wordToCheck = s.nextToken().toLowerCase().trim();
+                                            
+                            if(!dict.contains(wordToCheck))
+                            {
+                                System.out.println(wordToCheck);
+                            }
+                         }//inner loop
+                        
+                         sline = brStory.readLine();
+                             
+                }//outer loop
+    
+          
+          }//end try
+          catch (IOException e)
+          {
+             System.out.println("Error: " + e);
+          }
+            
    }//end main
 	
 
